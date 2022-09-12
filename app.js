9d01c32fa1259e38b2b56820cae333adaeb0352f
Aleto67 <aleto67@gmail.com>
2022-09-12 11:19:12 -0300

    Modificaiones hechas en la Mac

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 console.log('esto es una prueba');
 console.log('estoy probando la sincronizacion de archivos con git hub');
+console.log('esta modificación la hice en la Mac para seguir probando');
+
 
